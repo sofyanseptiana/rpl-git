b5cdf78d1fb316d30ed4f4fd146e955a3dccf0df
SOFYAN SEPTIANA <i16061@ftis>
2018-11-02 13:53:50 +0700

    Initial commit

---
+++ JavaApplication2/src/Calculator.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author i16061
+ */
+public class Calculator {
+
+    public Calculator() {
+    }
+    public float plus(float a,float b){
+    
+    }
+    public float minus(float a,float b){
+        
+        
+    }
+    public float multiply(float a,float b){
+        
+        
+    }
+    public float dibision(float a,float b){
+        
+        
+    }
+    
+}
